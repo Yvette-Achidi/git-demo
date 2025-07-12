d85579ef1647621af5315b6e86e281423c77cb03
Yvette Achidi <yvette.achidi23@gmail.com>
2025-07-12 15:52:59 -0700

    adding a bucket policy

--- s3.tf
+++ s3.tf
@@ -2,3 +2,8 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
     bucket = "my-s3-bucket"
 }
+# to create a bucket policy
+resource "aws_s3_bucket_policy" "my-s3-bucket-policy" {
+    bucket = aws_s3_bucket.my-s3-bucket.id
+    policy = data.aws_iam_policy_document.my-s3-bucket-policy.json
+}
